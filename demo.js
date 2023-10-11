295f56dff748aef394766cef1750fd7849f799b6
Sunee2023 <sunithavangalapudi15@gmail.com>
2023-10-11 16:16:00 +0530

    commit1

---
+++ demo.js
@@ -0,0 +1,19 @@
+
+<html>
+<body>
+
+<h1>Vasishta Automobiles</h1>
+<p>Best Automobile Store All Over India</p>
+
+<p id = 'demo'></p>
+
+<script>
+let carName1 = "Volvo XC60"; 
+let carName2 = 'Volvo XC90'; 
+
+document.getElementById("demo").innerHTML =
+carName1 + "  " + carName2; 
+</script>
+
+</body>
+</html>
